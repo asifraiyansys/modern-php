3480280accdd7462011e3f750a6f9c007ca9b22a
asifraiyan-systems <asifraiyan.systems@gmail.com>
2026-05-19 23:19:27 +0600

    Add PHP printing functions examples using echo and print

---
+++ basics/printing_fuctions/printing_functions.php
@@ -0,0 +1,22 @@
+<?php
+    echo "I am Asif Raiyan.\n";
+    echo ("I am a Software Engineer.\n");
+
+    print("I Love Programming.\n");
+    print "I am learning PHP.\n";
+
+    $fName = "Asif";
+    $lname = "Raiyan";
+
+    $fullName = $fName." ".$lname;
+    echo "My full name is $fullName.\n";
+
+    $userFirstName = 'asif';
+    $userLastName = 'raiyan';
+
+    $userFullName = $userFirstName." ".$userLastName;
+    echo "My User name is $userFullName.\n";
+
+
+
+?>
