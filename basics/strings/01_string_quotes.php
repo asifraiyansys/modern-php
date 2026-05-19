ab133b33bdf6a3d6686e0d088322895b3e6e1d28
asifraiyan-systems <asifraiyan.systems@gmail.com>
2026-05-19 23:40:39 +0600

    Add string_quotes.php for PHP string quotes examples

---
+++ basics/strings/01_string_quotes.php
@@ -0,0 +1,13 @@
+<?php
+// String double Quotes in PHP
+ $cityName = "Dhaka";
+
+ // String single Quotes in PHP
+ $countryName = 'Bangladesh';
+ 
+    echo "I live in $cityName, $countryName.\n"; 
+    echo 'The city name is: '.$cityName;
+    echo "\n";
+    echo  "the country name is: ".$countryName."\n";
+ 
+?>
