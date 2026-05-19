ddff202bacc9390945ff22051c156cbd64da9d66
asifraiyan-systems <asifraiyan.systems@gmail.com>
2026-05-19 22:42:13 +0600

    feat (basic) add PHP var_dump example

--- basics/variable/variable.php
+++ basics/variable/variable.php
@@ -7,4 +7,10 @@
     echo "Y = $y\n";
     echo "Z = $z\n";
 
+    var_dump(5);
+    var_dump("Asif Raiyan");
+    var_dump(5.5);
+    var_dump(true);
+    var_dump([10,20,30]);
+    var_dump(NULL);
 ?>
